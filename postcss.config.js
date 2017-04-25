1a6c4c29d2d7d0f6996b20d7763a39c2544dbcaf
inksmallfrog <inksmallfrog@gmail.com>
2017-04-25 17:37:17 +0800

    gh-pages-retry

--- postcss.config.js
+++ postcss.config.js
@@ -2,13 +2,12 @@
 * @Author: inksmallfrog
 * @Date:   2017-04-24 17:10:42
 * @Last Modified by:   inksmallfrog
-* @Last Modified time: 2017-04-24 17:18:38
+* @Last Modified time: 2017-04-25 17:36:42
 */
 
 'use strict';
 module.exports = {
   plugins: [
-    require('autoprefixer')({ /* ...options */ }),
     require('postcss-cssnext')
   ]
 }
